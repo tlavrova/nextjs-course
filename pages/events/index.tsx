39cbd196225f4d2643bdc57e8b94bafcd2f56ff7
Tatiana <tlavrova230@gmail.com>
2022-06-27 15:26:59 +0200

    Working on the All Events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,11 +1,11 @@
-import {getAllEvents} from "../../dummy-data";
+import {getAllEvents} from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import {Fragment} from "react";
 import {useRouter} from "next/router";
 
-const AllEventsPage = () => {
-    const events = getAllEvents()
+const AllEventsPage = (props: {events: any}) => {
+    const {events} = props
     const router = useRouter()
 
     const findEventsHandler = (year: string, month: string) => {
@@ -20,6 +20,17 @@ const AllEventsPage = () => {
             <EventList items={events}/>
         </Fragment>
     )
+}
+
+export async function getStaticProps() {
+    const events = await getAllEvents()
+
+    return {
+        props: {
+            events: events
+        },
+        revalidate: 60
+    }
 }
 
 export default AllEventsPage
