b9d8ea4405e1954bc83ac37459ea6dd4e033d9e7
Tatiana <tlavrova230@gmail.com>
2022-06-27 15:20:37 +0200

    Optimizing data fetching

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,21 +1,19 @@
-import {useRouter} from "next/router";
-import {getEventById} from "../../dummy-data";
+import {getEventById, getFeaturedEvents} from "../../helpers/api-util";
 import {Fragment} from "react";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
+import {GetStaticPropsContext} from "next";
 
-const EventDetailPage = () => {
-    const router = useRouter()
-    const eventId = router.query.eventId as string
-    const event = getEventById(eventId)
+const EventDetailPage = (props: {selectedEvent: any}) => {
+    const event = props.selectedEvent
 
     if (!event) {
         return (
-            <ErrorAlert>
-                <p>No event found!</p>
-            </ErrorAlert>
+            <div className='center'>
+                <p>Loading...</p>
+            </div>
         )
     }
 
@@ -28,6 +26,28 @@ const EventDetailPage = () => {
             </EventContent>
         </Fragment>
     )
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+    const eventId = context.params?.eventId as string
+    const event = await getEventById(eventId)
+
+    return {
+        props: {
+            selectedEvent: event
+        },
+        revalidate: 30
+    }
+}
+
+export async function getStaticPaths() {
+    const events = await getFeaturedEvents()
+    const paths = events.map(event => ({params: {eventId: event.id}}))
+
+    return {
+        paths: paths,
+        fallback: true
+    }
 }
 
 export default EventDetailPage
