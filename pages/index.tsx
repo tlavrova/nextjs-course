d2c2991f74a57505bf690e4ac7dbb97722ffea87
Tatiana <tlavrova230@gmail.com>
2022-06-28 13:40:12 +0200

    Configuring the head Metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import {getFeaturedEvents} from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 const HomePage = (props: { events: any }) => {
     return (
         <div>
+            <Head>
+                <title>NextJS Events</title>
+                <meta name="description" content="Find a lot of great events that allow you to evolve..."/>
+            </Head>
             <EventList items={props.events}/>
         </div>
     )
