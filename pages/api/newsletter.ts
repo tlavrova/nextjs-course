3eb37b83ccae7e4c86c9ae32bcca0f7e1f989bda
Tatiana <tlavrova230@gmail.com>
2022-07-14 10:47:24 +0200

    Adding error handling

--- pages/api/newsletter.ts
+++ pages/api/newsletter.ts
@@ -1,6 +1,20 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {MongoClient} from "mongodb";
 
+async function connectDatabase() {
+    console.log('test')
+    const client =  await MongoClient.connect(
+        'mongodb+srv://tatiana:STe6IzNBZlFKw4jo@cluster0.fmv6a.mongodb.net/events?retryWrites=true&w=majority'
+    )
+
+    return client
+}
+
+async function insertDocument (client: MongoClient, document: {email: string}) {
+    const db = client.db()
+    await db.collection('newsletter').insertOne(document)
+}
+
 async function handler (req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const userEmail = req.body.email
@@ -10,12 +24,22 @@ async function handler (req: NextApiRequest, res: NextApiResponse) {
             return
         }
 
-        const client =  await MongoClient.connect(
-            'mongodb+srv://tatiana:STe6IzNBZlFKw4jo@cluster0.fmv6a.mongodb.net/events?retryWrites=true&w=majority'
-        )
-        const db = client.db()
-        await db.collection('newsletter').insertOne({email: userEmail})
-        client.close()
+        let client
+
+        try {
+            client = await connectDatabase()
+        } catch (error) {
+            res.status(500).json({message: 'Connecting to the database failed!'})
+            return
+        }
+
+        try {
+            await insertDocument(client, {email: userEmail})
+            client.close()
+        } catch (error) {
+            res.status(500).json({message: 'Inserting data failed!'})
+            return
+        }
 
         res.status(201).json({message: 'Signed up!'})
     }
